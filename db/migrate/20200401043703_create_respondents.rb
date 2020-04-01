5ba504c588fb16b0c4b0c8966e6527f661cd2a32
Alistair A. Israel <aisrael@gmail.com>
2020-04-01 12:46:29 +0800

    rails g model Respondent identifier gender department

---
+++ db/migrate/20200401043703_create_respondents.rb
@@ -0,0 +1,11 @@
+class CreateRespondents < ActiveRecord::Migration[6.0]
+  def change
+    create_table :respondents do |t|
+      t.string :identifier
+      t.string :gender
+      t.string :department
+
+      t.timestamps
+    end
+  end
+end
